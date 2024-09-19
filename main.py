1abb4e8ce31029c149a5e6953a8b819d41aea976
Sarma Akondi <sarmaakondi@192-168-1-106.tpgi.com.au>
2024-09-19 12:49:02 +1000

    Create get all items enpoint

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+from api import router
 
-app = FastAPI()
+# Initialize the FastAPI app instance
+app = FastAPI(
+    title="Matching Algorithm API",
+    description="A RESTful API that takes user input, attempt to match it against a predefined list of items and return the best match along with a similarity score.",
+    version="0.1.0",
+)
+
+# Include the router in the app instance
+app.include_router(router, prefix="/api", tags=["Matching API"])
 
 
-@app.get("/")
+# Root endpoint
+@app.get("/", tags=["Root API"])
 def root():
     return {"message": "Hello from FastAPI 😁"}
